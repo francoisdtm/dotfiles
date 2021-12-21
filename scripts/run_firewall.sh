d991ca920194388a8f3784de6bac4845e0bae2d2
François Dautrême <4825058+francoisdtm@users.noreply.github.com>
2021-12-21 10:53:28 +0100

    Update run_firewall.sh

--- scripts/run_firewall.sh
+++ scripts/run_firewall.sh
@@ -8,3 +8,9 @@ sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on
 
 # Enable stealth mode
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+
+# Disallow signed built-in applications automatically
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned off
+
+# Disallow signed downloaded applications automatically
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
