5169c0f52a90e71143984f4a715373e7ffd563a1
François Dautrême <4825058+francoisdtm@users.noreply.github.com>
2021-12-21 10:48:53 +0100

    Create run_once_power_management.sh

---
+++ scripts/run_once_power_management.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Modify standby and power nap settings (mandatory if 'filevault.sh' enabled)
+sudo pmset -a powernap 0
+sudo pmset -a standby 0
+sudo pmset -a standbydelay 0
+sudo pmset -a autopoweroff 0
