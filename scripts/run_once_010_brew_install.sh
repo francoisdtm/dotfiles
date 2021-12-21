9a9cfaee5fa1c2701de17523fbc2e22a2a974271
François Dautrême <4825058+francoisdtm@users.noreply.github.com>
2021-12-21 16:54:31 +0100

    Update run_once_010_brew_install.sh

--- scripts/run_once_010_brew_install.sh
+++ scripts/run_once_010_brew_install.sh
@@ -15,3 +15,47 @@ brew upgrade
 # Tap repositories
 brew tap homebrew/cask
 brew tap homebrew/cask-fonts
+
+# Install packages
+brew install antigen
+brew install awscli
+brew install aws-sam-cli
+brew install browserpass
+brew install git
+brew install go
+brew install pass
+brew install python3
+brew install rustup
+brew install secretive
+brew install vercel-cli
+brew install zsh
+
+# Install casks
+brew install --cask app-cleaner
+brew install --cask binance
+brew install --cask brave-browser
+brew install --cask discord
+brew install --cask docker
+brew install --cask font-fira-code
+brew install --cask font-jetbrains-mono
+brew install --cask gpg-suite-no-mail
+brew install --cask intellij-idea
+brew install --cask iterm2
+brew install --cask microsoft-excel
+brew install --cask microsoft-powerpoint
+brew install --cask microsoft-teams
+brew install --cask microsoft-word
+brew install --cask minecraft
+brew install --cask ngrok
+brew install --cask notion
+brew install --cask postman
+brew install --cask proxyman
+brew install --cask raspberry-pi-imager
+brew install --cask telegram
+brew install --cask tradingview
+brew install --cask transmission
+brew install --cask vscodium
+brew install --cask vmware-fusion
+brew install --cask windscribe
+brew install --cask wireshark
+brew install --cask zerotier-one
