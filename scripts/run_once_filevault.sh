b22200ba38d35b9ac52cb5b6e6996eb1f1b4198a
François Dautrême <4825058+francoisdtm@users.noreply.github.com>
2021-12-21 10:44:15 +0100

    Create run_once_filevault.sh

---
+++ scripts/run_once_filevault.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Enable Filevault
+sudo fdesetup enable
+
+# Enforce system hibernation and evict FileVault keys from memory instead of traditional sleep to memory
+sudo pmset -a destroyfvkeyonstandby 1
+sudo pmset -a hibernatemode 25
