4b464b170536db03ce3c762b59b466faf9837dd3
François Dautrême <4825058+francoisdtm@users.noreply.github.com>
2021-12-24 12:55:24 +0100

    Delete run_once_004_spotlight.sh

--- .chezmoiscripts/run_once_004_spotlight.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Disable Spotlight Suggestions in both the Spotlight preferences and Safari's Search preferences to avoid your search queries being sent to Apple."
-echo "Press enter once done..."
-read -n 1
-
-echo "Also disable Bing Web Searches in the Spotlight preferences to avoid your search queries being sent to Microsoft."
-echo "Press enter once done..."
-read -n 1
